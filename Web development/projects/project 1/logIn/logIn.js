536a37d02a1a00d3621e13662db030277e9e983f
Abhishek kumar <92434596+Abhishek0ii@users.noreply.github.com>
2023-06-18 14:32:17 +0530

    Add files via upload

---
+++ Web development/projects/project 1/logIn/logIn.js
@@ -0,0 +1,25 @@
+// // const signUpButton = document.getElementById('signUp')
+// // const signInButton = document.getElementById('signIp')
+// // const container = document.getElementById('container')
+
+// // signUpButton.assEventlistener('click',() => {
+// //     container.classList.add("right-panel-active")
+// // })
+
+// // signInButton.assEventlistener('click',() => {
+// //     container.classList.remove("right-panel-active")
+// // })
+
+
+
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
+
+// signUpButton.addEventListener('click', () => {
+//     container.classList.add("right-panel-active");
+// });
+
+// signInButton.addEventListener('click', () => {
+//     container.classList.remove("right-panel-active");
+// });
